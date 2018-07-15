00e9910bceb962da6febce2770f32f3c892fd23b
Doug Cooper <dougcooper@MiMac2.local>
2018-07-14 21:30:59 -0400

    reverted back to dist nameing

--- config/index.js
+++ config/index.js
@@ -4,8 +4,8 @@ var path = require('path')
 module.exports = {
   build: {
     env: require('./prod.env'),
-    index: path.resolve(__dirname, '../gh_pages/index.html'),
-    assetsRoot: path.resolve(__dirname, '../gh_pages'),
+    index: path.resolve(__dirname, '../dist/index.html'),
+    assetsRoot: path.resolve(__dirname, '../dist'),
     assetsSubDirectory: 'static',
     assetsPublicPath: '/',
     productionSourceMap: true,
